eaad603f4b4011e6e0e8980c93e515fc4f2768a3
!skhak <nurlanjunushaliev76@icloud.com>
2025-08-30 20:27:03 +0600

    iskhak/header/footer

--- src/widgets/footer/Footer.tsx
+++ src/widgets/footer/Footer.tsx
@@ -1,7 +1,58 @@
 import React from "react";
 import scss from "./Footer.module.scss";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+
 const Footer = () => {
-  return <footer className={scss.footer}>footer</footer>;
+  return (
+    <footer className={scss.footer}>
+      <div className="container">
+        <div className={scss.content}>
+          <div className={scss.logo}>
+            <img src="/Logo.svg" alt="" />
+            <img src="/preLogo.svg" alt="" />
+          </div>
+          <ul>
+            <li>Jogos</li>
+            <li>Crash</li>
+            <li>Double</li>
+            <li>Mines</li>
+            <li>Dice</li>
+            <li>Tower</li>
+          </ul>
+          <ul>
+            <li>Links</li>
+            <li>Conta</li>
+            <li>Depositar</li>
+            <li>Sacar</li>
+          </ul>
+          <ul>
+            <li>Social</li>
+            <li>
+              <span>
+                <FaFacebookF />
+              </span>
+              Facebook
+            </li>
+            <li>
+              <span>
+                <FaTwitter />
+              </span>
+              Twitter
+            </li>
+            <li>
+              <span>
+                <FaInstagram />
+              </span>
+              Instagram
+            </li>
+          </ul>
+        </div>
+        <hr />
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 import scss from "./Header.module.scss";
+import { CiSearch } from "react-icons/ci";
+import { MdOutlineCloudDownload } from "react-icons/md";
+
 const Header = () => {
-  return <header className={scss.header}>header iskhak</header>;
+  return (
+    <header className={scss.header}>
+      <div className="container">
+        <div className={scss.content}>
+          <button>
+            <CiSearch />
+          </button>
+          <p>pesquisar</p>
+          <div className={scss.option}>
+            <select>
+              <option value="en">EN</option>
+              <option value="ru">RU</option>
+            </select>
+            <button className={scss.entrar}>Entrar</button>
+            <button>
+              Cadastrar <MdOutlineCloudDownload style={{ fontSize: "20px" }} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
